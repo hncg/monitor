4f5fe1319af0a44de842601a2869aa2bc623d338
hncg <574878649@qq.com>
2016-05-06 00:05:24 +0800

    删除prc 文件，设置env文件位置

--- monitor/handler/__init__.py
+++ monitor/handler/__init__.py
@@ -1,8 +1,9 @@
 # coding=utf-8
 import sys
-sys.path.append('../../')
+sys.path.append('../')
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 import env
+print env.socket
 engine = create_engine('mysql://cg:123456@localhost:3306/rednet?charset=utf8&unix_socket=' + env.socket, echo=False) # noqa
 DBSession = sessionmaker(bind=engine)
